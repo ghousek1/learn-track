a8754a2eb43b4e4f260e942154c4fdbc1617a819
ghousek1 <ghousek1@outlook.com>
2025-12-22 15:25:23 +0530

    ID Generator is improved with static fields

--- src/main/java/com/airtribe/learntrack/util/IdGenerator.java
+++ src/main/java/com/airtribe/learntrack/util/IdGenerator.java
@@ -1,9 +1,20 @@
 package com.airtribe.learntrack.util;
 
 public class IdGenerator {
-    private static int counter = 1;
 
-    public static int nextId() {
-        return counter++;
+    private static int studentIdCounter = 1;
+    private static int courseIdCounter = 1;
+    private static int enrollmentIdCounter = 1;
+
+    public static int getNextStudentId() {
+        return studentIdCounter++;
+    }
+
+    public static int getNextCourseId() {
+        return courseIdCounter++;
+    }
+
+    public static int getNextEnrollmentId() {
+        return enrollmentIdCounter++;
     }
 }
